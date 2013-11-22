9293e4978a4d53c8c741ec57b426b80a86a3d38f
arriej <info@blockmadness,com>
2013-11-22 21:59:11 +0100

    First commit Checking command arguments

--- src/com/blockmadness/kcin/madrank/Commands.java
+++ src/com/blockmadness/kcin/madrank/Commands.java
@@ -16,17 +16,17 @@ public class Commands implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(cmd.getName().equalsIgnoreCase("playtime") | cmd.getName().equalsIgnoreCase("age")) { // If the player typed /playtime or /age
 			/* Check for arguments using
-			 * 
-			 * if(args.length > 1) {
-			 * 	sender.sendMessage("Too many arguments!");
-			 * 	return false;
-			 * } elseif(args.length == 1) {
-			 * 	// they typed in a player name after the command (or something else, but it's something!)
-			 * } elseif(args.length < 1) {
-			 * 	// they didn't type anything after the command, so send them their playtime
-			 *	// don't forget to check if it's a player or the console ofc (see below)
-			 * }
-			 * 
+			 */
+			if(args.length > 1) {
+				sender.sendMessage("Too many arguments!");
+				return false;
+			} else if(args.length == 1) {
+				// they typed in a player name after the command (or something else, but it's something!)
+			} else if(args.length < 1) {
+				 // they didn't type anything after the command, so send them their playtime
+				 // don't forget to check if it's a player or the console ofc (see below)
+			}
+			 /* 
 			 * 
 			 * If no arguments then make sure to check if it's a player or the console using
 			 *
